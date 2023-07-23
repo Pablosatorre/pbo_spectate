6761a1a18d06424ea790d5e972234ad1aeb14507
Pablo Carmona <80336428+Pablosatorre@users.noreply.github.com>
2023-07-23 18:11:34 +0200

    Update main.lua
    Fixed to works with OneSync Infinity

--- pbo_spectate/server/main.lua
+++ pbo_spectate/server/main.lua
@@ -1,4 +1,4 @@
-PBO = nil
+pbo = nil
 print(
     Config.Prename ..
         ' '..
@@ -10,12 +10,12 @@ scriptname = "^1SPECTATE^7: "
 TriggerEvent(
     "esx:getSharedObject",
     function(obj)
-        PBO = obj
+        pbo = obj
     end
 )
 
-PBO.RegisterServerCallback('pbo_spectate:checkAdmin', function(source, cb)
-    local xPlayer = PBO.GetPlayerFromId(source)
+pbo.RegisterServerCallback('mdn_spectate:checkAdmin', function(source, cb)
+    local xPlayer = pbo.GetPlayerFromId(source)
     local g = xPlayer.getGroup()
     cb(g)
 end)
@@ -25,85 +25,98 @@ TriggerEvent(
     "menuspectate",
     "admin",
     function(source, args, user)
-        TriggerClientEvent("pbo_spectate:spectate", source, target)
+        TriggerClientEvent("pbo_spectateplayers:spectate", source, target)
     end,
     function(source, args, user)
         TriggerClientEvent("chatMessage", source, scriptname, {255, 0, 0}, "Permisos Insuficientes")
     end
 )
 
-PBO.RegisterServerCallback(
-    "pbo_spectate:getPlayerData",
+pbo.RegisterServerCallback(
+    "pbo_spectateplayers:getPlayerData",
     function(source, cb, id)
-        local xPlayer = PBO.GetPlayerFromId(id)
+        local xPlayer = pbo.GetPlayerFromId(id)
         if xPlayer ~= nil then
             cb(xPlayer)
         end
     end
 )
 
-RegisterServerEvent("pbo_spectate:kick")
+RegisterServerEvent("pbo_spectateplayers:kick")
 AddEventHandler(
-    "pbo_spectate:kick",
+    "pbo_spectateplayers:kick",
     function(target, msg)
-        local xPlayer = PBO.GetPlayerFromId(source)
+        local xPlayer = pbo.GetPlayerFromId(source)
 
         if xPlayer.getGroup() ~= "user" then
             DropPlayer(target, msg)
         else
-            print(("pbo_spectate: %s intento kickear a un jugador"):format(xPlayer.identifier))
-            DropPlayer(source, "pbo_spectate: no estas autorizado para kickear a un jugador gilipollas!")
+            print(("pbo_spectateplayers: %s intento kickear a un jugador"):format(xPlayer.identifier))
+            -- DropPlayer(source, "pbo_spectateplayers: no estas autorizado para kickear a un jugador gilipollas!")
         end
     end
 )
 
-PBO.RegisterServerCallback(
-    "pbo_spectate:getOtherPlayerData",
-    function(source, cb, target)
-        local xPlayer = PBO.GetPlayerFromId(target)
-        if xPlayer ~= nil then
-            local identifier = GetPlayerIdentifiers(target)[1]
+pbo.RegisterServerCallback("pbo_spectateplayers:getOtherPlayerData", function(source, cb, target)
+    local xPlayer = pbo.GetPlayerFromId(target)
 
-            local result =
-                MySQL.Sync.fetchAll(
-                "SELECT * FROM users WHERE identifier = @identifier",
-                {
-                    ["@identifier"] = identifier
-                }
-            )
+    if xPlayer then
+        local result = MySQL.Sync.fetchAll("SELECT * FROM users WHERE identifier = @identifier", {
+            ["@identifier"] = xPlayer.identifier
+        })
 
-            local user = result[1]
-            local firstname = user["firstname"]
-            local lastname = user["lastname"]
-            local sex = user["sex"]
-            local dob = user["dateofbirth"]
-            local height = user["height"] .. " Centimetri"
-            local money = user["money"]
-            local bank = user["bank"]
+        local sex = result[1].sex == "m" and "Hombre" or "Mujer"
+        local dob = result[1].dateofbirth
+        local height = result[1].height
 
-            local data = {
-                name = GetPlayerName(target),
-                job = xPlayer.job,
-                inventory = xPlayer.inventory,
-                accounts = xPlayer.accounts,
-                weapons = xPlayer.loadout,
-                firstname = firstname,
-                lastname = lastname,
-                sex = sex,
-                dob = dob,
-                height = height,
-                money = money,
-                bank = bank
-            }
+        local data = {
+            name = GetPlayerName(target),
+            job = xPlayer.job,
+            inventory = xPlayer.inventory,
+            accounts = xPlayer.accounts,
+            weapons = xPlayer.loadout,
+            charName = xPlayer.getName(),
+            sex = sex,
+            dob = dob,
+            height = height,
+            money = xPlayer.getMoney(),
+            bank = xPlayer.getAccount('bank').money
+        }
 
-            TriggerEvent(
-                "esx_license:getLicenses",
-                target,
-                function(licenses)
-                    data.licenses = licenses
-                    cb(data)
-                end
-            )
-        end
+        TriggerEvent(
+            "esx_license:getLicenses",
+            target,
+            function(licenses)
+                data.licenses = licenses
+                cb(data)
+            end
+        )
     end
-)
+end)
+
+pbo.RegisterServerCallback('pbo_spectateplayers:getPlayersList', function(source, cb)
+    local xPlayers = pbo.GetPlayers()
+    local players = {}
+
+    for i = 1, #xPlayers, 1 do
+        local xPlayer = pbo.GetPlayerFromId(xPlayers[i])
+
+        table.insert(players, {
+            id = xPlayer.source,
+            name = GetPlayerName(xPlayer.source),
+        })
+    end
+
+    cb(players)
+end)
+
+pbo.RegisterServerCallback('spectate:requestPlayerCoords', function(source, cb, target)
+    local xPlayer = pbo.GetPlayerFromId(source)
+    local coords = GetEntityCoords(GetPlayerPed(target))
+
+    if xPlayer.getGroup() == 'user' then
+        return
+    end
+
+    cb(coords)
+end)
