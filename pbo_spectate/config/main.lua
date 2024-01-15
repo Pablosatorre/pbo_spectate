dcd278a94cd80ed4ffd638160359a1ea1ef3abdf
Pablo Carmona <80336428+Pablosatorre@users.noreply.github.com>
2024-01-15 20:24:34 +0100

    Update main.lua

--- pbo_spectate/config/main.lua
+++ pbo_spectate/config/main.lua
@@ -2,10 +2,3 @@ Config = {}
 
 Config.CambiarKey = true
 
--- Source Configs
-
-Config.Prename      = '[ ^1pbo CORE^7 ]'
-
-Config.Script       = ' | ^3pbo_SPECTATEPLAYERS^7 | '
-
-Config.PrintConsole = '^5Script Started Succesfully'
